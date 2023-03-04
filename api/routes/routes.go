567e48aaeabf15b008186403f269d960176a1f59
ryotaryota0212 <ryota.kandai.0212@i.softbank.jp>
2023-03-05 01:00:34 +0900

    ごみ、県などのルーティング追加

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,16 +1,27 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"api/controllers"
+	"context"
+
+	firebase "firebase.google.com/go"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
+)
 
 func Server() {
 	route := gin.Default()
 	route.GET("/", Top)
 	route.GET("/user", User)
+	route.GET("/login", Login)
+	route.GET("/prefectures", controllers.Prefectures)
+	route.GET("/prefectures/:id", controllers.Municipality)
+	route.GET("/garbage/:id", controllers.FetchGarbage)
 	route.Run()
 }
 func Top(cont *gin.Context) {
 	cont.JSON(200, gin.H{
-		"message": "!!!!",
+		"message": "!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
 	})
 }
 func User(cont *gin.Context) {
@@ -18,3 +29,14 @@ func User(cont *gin.Context) {
 		"message": "user",
 	})
 }
+func Login(cont *gin.Context) {
+	opt := option.WithCredentialsFile("../public/auth/firebase-auth.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return
+	}
+	cont.JSON(200, gin.H{
+		"app": app,
+	})
+}
+
