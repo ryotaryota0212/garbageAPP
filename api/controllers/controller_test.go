592ea230755604b6e1dc836834ca3bd0a0ae35b5
ryotaryota0212 <ryota.kandai.0212@i.softbank.jp>
2023-03-05 23:59:35 +0900

    テスト実装

---
+++ api/controllers/controller_test.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"api/database"
+	"testing"
+)
+
+func TestFetchGarbage(t *testing.T) {
+	db := database.ConnectDB()
+
+	_,err := db.Query("select * from collections where local_number = 232017" )
+	if err != nil {
+		t.Errorf("not select collection by %v", err)
+    }
+}
