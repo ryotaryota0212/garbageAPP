264125a0e55ebb75f669f79ea43c4165c9f929fe
ryotaryota0212 <ryota.kandai.0212@i.softbank.jp>
2023-03-06 00:00:44 +0900

    ログインルーティング追加

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"api/controllers"
 	"context"
+	"fmt"
 
 	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
@@ -13,7 +14,7 @@ func Server() {
 	route := gin.Default()
 	route.GET("/", Top)
 	route.GET("/user", User)
-	route.GET("/login", Login)
+	route.GET("/login/:id", Login)
 	route.GET("/prefectures", controllers.Prefectures)
 	route.GET("/prefectures/:id", controllers.Municipality)
 	route.GET("/garbage/:id", controllers.FetchGarbage)
@@ -30,12 +31,24 @@ func User(cont *gin.Context) {
 	})
 }
 func Login(cont *gin.Context) {
+	id := cont.Param("id")
+	ctx := context.Background()
 	opt := option.WithCredentialsFile("../public/auth/firebase-auth.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
+	client, e := app.Auth(ctx)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+	fmt.Println(client)
+	user := controllers.GetUser(ctx, client, id)
+	fmt.Println(user)
 	cont.JSON(200, gin.H{
+		"user": user.UserInfo,
 		"app": app,
 	})
 }
