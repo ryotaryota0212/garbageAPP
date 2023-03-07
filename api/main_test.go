e09a4c6d950f3733e22cafa28309dc7141db08d6
ryotaryota0212 <ryota.kandai.0212@i.softbank.jp>
2023-03-07 22:58:01 +0900

    mainテスト

--- api/main_test.go
+++ api/main_test.go
@@ -1,1 +1,14 @@
 package main_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(t *testing.T)  {
+	fmt.Println("test")
+	test := 20
+	if test != 20 {
+		t.Errorf("not equal 20 %v", test)
+	}
+}
