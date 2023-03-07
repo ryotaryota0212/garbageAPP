bcfb8332ff31f685e6a6654ab3167af5ba99e44e
ryotaryota0212 <ryota.kandai.0212@i.softbank.jp>
2023-03-07 22:37:55 +0900

    connect追加

---
+++ api/database/connect.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+func ConnectDB() *sql.DB {
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		panic(err)
+	}
+	c := mysql.Config{
+		DBName:    "go_mysql",
+		User:      "admin",
+		Passwd:    "admin",
+		Addr:      "localhost:3306",
+		Net:       "tcp",
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+	db, _ := sql.Open("mysql", c.FormatDSN())
+	return db
+}
